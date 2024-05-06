05fdcbcdd3a39319ddb8fe3b5443daf75ec5e055
Andres2800 <candres.calle@ucuenca.edu.ec>
2024-05-05 23:06:43 -0500

    Definiciones
    Se agregaron las definiciones correspondientes a las variables de entrada y salida.

--- Practica_CD/src/main.cpp
+++ Practica_CD/src/main.cpp
@@ -2,19 +2,31 @@
 #include <modulo.h>
 //Declarar los leds con enums
 //Todo lo que no sea setup y loop va a un archivo externo
+#define LED_GREEN 2
+#define LED_YELLOW 3
+#define LED_RED 4
+#define LED_BLUE 5
+#define SW1 6
+#define SW2 7
+#define SW3 8
+#define SW4 9
 
-int del = 20;
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+  pinMode(SW3, INPUT);
+  pinMode(SW4, INPUT);
 }
 
 
 
 void loop() {
   encenderLed(13);
-  delay(del);
+  delay(500);
   apagarLed(13);
-  delay(del);
+  delay(500);
   }
