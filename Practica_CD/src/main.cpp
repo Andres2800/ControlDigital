f8464dafe1ee9a6426048c0bd378686a1238d44e
Andres2800 <candres.calle@ucuenca.edu.ec>
2024-05-06 19:38:27 -0500

    Agregado secuencias
    Se agregaron las dos secuencias de encendido de los led, también como la lectura de los switches que permiten cambiar la secuencia a utilizar.

--- Practica_CD/src/main.cpp
+++ Practica_CD/src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 #include <modulo.h>
-//Declarar los leds con enums
-//Todo lo que no sea setup y loop va a un archivo externo
 #define LED_GREEN 9
 #define LED_YELLOW 8
 #define LED_RED 7
@@ -12,6 +10,10 @@
 #define SW4 2
 uint16_t retraso = 200;
 unsigned long prev_time = 0;
+int contador = 1;
+const uint16_t secuencia1[] = {LED_GREEN, LED_YELLOW, LED_RED, LED_BLUE};
+const uint16_t secuencia2[] = {LED_BLUE, LED_RED, LED_YELLOW, LED_GREEN};
+bool sentido = true;
 
 void setup() {
   pinMode(LED_GREEN, OUTPUT);
@@ -26,18 +28,23 @@ void setup() {
 }
 
 
-
 void loop() {
   unsigned long current_time = millis();
   if((current_time - prev_time) >= retraso){
     prev_time = current_time;
-    Serial.println(retraso);
-
+    contador ++;
+    bool ledsa = apagarLeds();
+    if (sentido){bool ledsp = encenderLed(secuencia1[contador%4]);}
+    else{bool ledsp = encenderLed(secuencia2[contador%4]);}
   }
+  
  bool tecla1 = leerTecla(SW1);
  bool tecla2 = leerTecla(SW2);
+ bool tecla3 = leerTecla(SW3);
+ bool tecla4 = leerTecla(SW4);
  if(tecla1){retraso = 200;}
  if(tecla2){retraso = 750;}
-
+ if(tecla3){sentido = true;}
+ if(tecla4){sentido = false;}
 
   }
