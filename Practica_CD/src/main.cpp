f6574abb754b1e21c1fcbf727f8a07ea3f43d5cd
Andres2800 <candres.calle@ucuenca.edu.ec>
2024-06-02 20:21:46 -0500

    Comentado
    Se agregaron comentarios que describen el funcionamiento de todo el programa así como la librería creada.

--- Practica_CD/src/main.cpp
+++ Practica_CD/src/main.cpp
@@ -1,8 +1,8 @@
-#include <Arduino.h>
-#include <modulo.h>
+#include <Arduino.h>   //Inlcusion del modulo con las funciones de Arduino
+#include <modulo.h>   // Inclusion del modulo creado para la practica
 
 
-
+//Declaracion de los pins conectados a los leds y botones
 #define LED_GREEN 9
 #define LED_YELLOW 8
 #define LED_RED 7
@@ -11,13 +11,14 @@
 #define SW2 4
 #define SW3 3
 #define SW4 2
-uint16_t retraso = 200;
-unsigned long prev_time = 0;
-int contador = 1;
-const uint16_t secuencia[] = {LED_GREEN, LED_YELLOW, LED_RED, LED_BLUE};
-bool sentido = true;
-
+uint16_t retraso = 200;       //Tiempo de encendido de los leds
+unsigned long prev_time = 0;  //Variable utilizada para el retardo no bloqueante
+int contador = 1;             //Variable que indica que posicion de la secuencia encender
+const uint16_t secuencia[] = {LED_GREEN, LED_YELLOW, LED_RED, LED_BLUE};  //Secuencia de encendido de leds
+bool sentido = true;          //Sentido de recorrido de la secuencia 
+//
 void setup() {
+  //Declaracion de los pins de entrada y salida
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_RED, OUTPUT);
@@ -26,29 +27,30 @@ void setup() {
   pinMode(SW2, INPUT);
   pinMode(SW3, INPUT);
   pinMode(SW4, INPUT);
-  Serial.begin(9600);
 }
 
 
 void loop() {
-  unsigned long current_time = millis();
-  if((current_time - prev_time) >= retraso){
+  unsigned long current_time = millis();            //Valor utilizado para el retardo no bloqueante
+  ///////////////////////////// Retardo no bloqueante  /////////////////////////////
+  if((current_time - prev_time) >= retraso){    
     prev_time = current_time;    
-    bool ledsa = apagarLeds();
-    bool ledsp = encenderLed(secuencia[contador]);
-    if (sentido){contador ++;}else{contador --;}    
-    if (contador >= 4){contador = 0;}
-    if (contador < 0){contador = 3;}
+    bool ledsa = apagarLeds();                      //Llamado a la funcion que apaga todos los leds
+    bool ledsp = encenderLed(secuencia[contador]);  //Llamado a la funcion que enciende un led de la secuencia
+    if (sentido){contador ++;}else{contador --;}    //Recorrido de la secuencia de acuerdo al sentido
+    if (contador >= 4){contador = 0;}               //Reinicio de la secuencia 
+    if (contador < 0){contador = 3;}                //Reinicio de la secuencia 
     
   }
   
- bool tecla1 = leerTecla(SW1);
- bool tecla2 = leerTecla(SW2);
- bool tecla3 = leerTecla(SW3);
- bool tecla4 = leerTecla(SW4);
- if(tecla1){retraso = 200;}
- if(tecla2){retraso = 750;}
- if(tecla3){sentido = true;}
- if(tecla4){sentido = false;}
+///////////////////////////// Lectura de los botones  /////////////////////////////
+ bool tecla1 = leerTecla(SW1);    //Llamado a la funcion de lectura para el boton 1
+ bool tecla2 = leerTecla(SW2);    //Llamado a la funcion de lectura para el boton 2
+ bool tecla3 = leerTecla(SW3);    //Llamado a la funcion de lectura para el boton 3
+ bool tecla4 = leerTecla(SW4);    //Llamado a la funcion de lectura para el boton 4
+ if(tecla1){retraso = 200;}       //Tiempo de encendido de 200ms al presionar el boton 1
+ if(tecla2){retraso = 750;}       //Tiempo de encendido de 700ms al presionar el boton 2
+ if(tecla3){sentido = true;}      //Cambio de sentido de la secuencia al presionar el boton 3
+ if(tecla4){sentido = false;}     //Cambio de sentido de la secuencia al presionar el boton 4
 
   }
--- Practica_CD/src/modulo.c
+++ Practica_CD/src/modulo.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <Arduino.h>
 
-//extern "C"{#include "modulo.h"}
 
 int anterior;
 bool valor;
 
 bool encenderLed(uint16_t led){
+  /// Funcion que prende el led que tiene como entrada 
+  /// Retorna verdadero cuando se realizó correctamente
+  /// Retorna falso cuando no pudo realizarse
     digitalWrite(led, 1);
     return true;
 }
 bool apagarLeds(){
+  /// Funcion que apaga todos los leds 
+  /// Retorna verdadero cuando se realizó correctamente
+  /// Retorna falso cuando no pudo realizarse
     digitalWrite(6,0);
     digitalWrite(7,0);
     digitalWrite(8,0);
@@ -20,9 +25,11 @@ bool apagarLeds(){
 
 
 bool leerTecla(uint16_t tecla){
+  /// Funcion que lee cuando se presiona un boton 
+  /// Retorna verdadero cuando se presiono el boton
+  /// Retorna falso cuando no se presiono el boton
 
   int estado = digitalRead(tecla);
-  
   if (anterior==HIGH && estado == LOW){
     valor = true;
   }else{
